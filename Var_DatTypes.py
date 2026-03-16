782ce7999dc72185cb407c93f5b604a16cefdbb0
Rishab Raj <rr0948555@gmail.com>
2026-03-16 21:16:34 +0530

    Python

---
+++ Var_DatTypes.py
@@ -0,0 +1,11 @@
+# Variables are container to store the data
+name = "Alex"
+age = 20
+isStudent = True
+height = 5.9
+
+
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", isStudent)
+print("Height:", height)
