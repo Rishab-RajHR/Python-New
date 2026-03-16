1d022b3c79800f702e11ce3c08422716e9b478f4
Rishab Raj <rr0948555@gmail.com>
2026-03-16 21:29:46 +0530

    Typecasting

--- Var_DatTypes.py
+++ Var_DatTypes.py
@@ -9,3 +9,55 @@ print("Name:", name)
 print("Age:", age)
 print("Is Student:", isStudent)
 print("Height:", height)
+
+# Rules for naming convention
+# 1. No - Number
+# 2. Yes - Meaningful Letters or _
+# 3. No - ! @ #
+# 4. use Lowercase and underscore
+# 5. ihsbsjknslsgdn, x , y, z
+
+
+
+# Number Data Type
+# 1. Integer
+age = 30
+print("Age:", age)
+print(type(age))
+# 2. Float
+pi = 3.14
+print("Pi:", pi)
+print(type(pi))
+
+
+# 2. String
+name = "Mohit"
+print("name")
+print("Name:", name)
+print(type(name))
+
+
+# 3. Boolean
+isStudent = True
+print("Is Student:", isStudent)
+print(type(isStudent))
+
+
+
+# Dynamic typing means python detects the data type on its own
+
+
+
+# Interview Questions
+# None
+noting = None
+print("None:", noting)
+print(type(noting))
+
+
+
+# Typecasting converting from one data type to another
+x = "10"
+a = int(x)  #Forcefully convert string to integer
+y = 20.25
+print(x+y)
