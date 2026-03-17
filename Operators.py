adf80d99c071749ba5d2b8defd471542ba22fc3f
Rishab Raj <rr0948555@gmail.com>
2026-03-17 15:03:41 +0530

    Operatoprs

---
+++ Operators.py
@@ -0,0 +1,59 @@
+# Arithmetic Operator
+num1 = 20
+num2 = 10
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2) # Converts to nearest whole number
+print("Modulus:", num1 % num2) 
+print("Exponent:", num1 ** num2) 
+
+
+
+# Comparison Operator // Relational Operator
+num1 = 2
+num2 = 4
+print("Equal to:", num1 == num2)
+print("Not equal to:", num1 != num2)
+print("Greater than:", num1 > num2)
+print("Less than:", num1 < num2)
+print("Greater than or equal to:", num1 >= num2)
+print("Less than or equal to:", num1 <= num2)
+
+
+
+
+# Logical Operator
+num1 = True
+num2 = False
+print("Logical AND:", num1 and num2)
+print("Logical OR:", num1 or num2)
+print("Logical NOT:",not num1)
+
+
+
+
+# Assignment Operator
+num = 2
+# num1 = 4
+# num = num + 2
+
+# Addition
+# num += num
+# print("num:", num)
+
+# Subtraction
+# num -= num
+# print("num:", num)
+
+# Multiplication
+# num *= num
+# print("num:", num)
+
+# Division
+# num /= num
+# print("num:", num)
+
+
+# Bitwise Operator
