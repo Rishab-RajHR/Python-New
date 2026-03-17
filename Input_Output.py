d276073df20370a926f91fc832d771c360d413b7
Rishab Raj <rr0948555@gmail.com>
2026-03-17 14:46:39 +0530

    Formatting

--- Input_Output.py
+++ Input_Output.py
@@ -7,3 +7,32 @@ Multiline Comments
 # Output Methods
 # name = "Alex"
 # print("Name:", name)
+
+
+# Input Methods
+# name = input("Enter your name: ")
+# print("Name:", name)
+# print("Type of name:", type(name))  --> string
+
+# age = input("Enter your age: ")
+# age = int(age) # Convert the input to an integer
+# print("Age:", age)
+# print("Type of age:", type(age))
+
+
+# age = int(input("Enter your age: "))
+# print("Age:", age)
+# print("Type of age:", type(age))
+
+
+# Formatting output
+name = "Alex"
+age = 29
+print("My Name is",name,"and my age is",age)
+
+# f-string
+print(f"My Name is {name} and my age is {age}")
+
+
+# format method
+print("My Name is {} and my age is {}".format(name,age))
