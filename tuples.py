29294dba0a811308fdd3c3a8bc34765939681a3d
Rishab Raj <rr0948555@gmail.com>
2026-03-20 22:59:29 +0530

    Tuple

---
+++ tuples.py
@@ -0,0 +1,36 @@
+# A tuple is a built-in data type used to store a collection of items. 
+# Immutable cannot be changed after creation , Allows duplicates, Can different data types.
+
+# Creating tuple
+num = (10,30,15,56,"Alex")
+print(num)
+print(type(num))
+
+# num = (10) => single value cannot be tuple
+
+
+
+# Tuple is immutable cannot be changed  
+
+# num = (10,30,15,56)
+# num[0] = 100
+# print(num)
+
+
+# Slicing tuple
+
+num2 = (10,20,30,40,50)
+
+print(num[1:3])
+
+
+
+# Tuple Methods
+
+num = (2,11,10,30,10)
+
+# print(num.index(11))
+print(num.count(10))
+
+
+
