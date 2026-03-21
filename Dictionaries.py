cc5a19798177a01d21a8e3248b2e9e655cfc1956
Rishab Raj <rr0948555@gmail.com>
2026-03-21 22:30:40 +0530

    Updating the value

--- Dictionaries.py
+++ Dictionaries.py
@@ -10,5 +10,39 @@ student = {
    "age" : 20,
    "passed": True,
     "course": "Python Programming",
+    "marks": [85, 90, 78, 92],
+    "subjects": ("Python", "Data Structures", "Algorithm"),
 }
 print("Student Details", student)
+print(type(student))
+print(len(student))
+
+
+
+# For accessing the Dictionaries
+student1 = {
+   "name" : "Alex Pandian",
+   "age" : 20,
+   "passed": True,
+    "course": "Python Programming",
+    "marks": [85, 90, 78, 92],
+    "subjects": ("Python", "Data Structures", "Algorithm"),
+}
+
+print(student1["name"])
+print(student1["subjects"])
+print(student1.get("subjectData"))   # Will give none if not found
+
+
+
+
+# For updating values in Dictionaries
+student2 = {
+   "name" : "Alex Pandian",
+   "age" : 20,
+   "passed": True,
+    "course": "Python Programming",
+}
+student2["marks"] = 95
+print(student2)
+ 
