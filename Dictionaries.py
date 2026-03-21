40e2e396133ad7ed1bd2aa04fd3b7b9450601f95
Rishab Raj <rr0948555@gmail.com>
2026-03-21 22:21:29 +0530

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,14 @@
+# A dictionary is a key–value data structure in Python.
+# word : meaning => key : value
+# It is unordered list
+# There is unique key
+
+
+# Creating Dicitionaries
+student = {
+   "name" : "Alex Pandian",
+   "age" : 20,
+   "passed": True,
+    "course": "Python Programming",
+}
+print("Student Details", student)
