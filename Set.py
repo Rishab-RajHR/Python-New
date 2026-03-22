69851b530deef3e32d98ead9caa4a395e799b4ce
Rishab Raj <rr0948555@gmail.com>
2026-03-22 14:39:01 +0530

    Interscetion

--- Set.py
+++ Set.py
@@ -43,5 +43,20 @@ print(num1)
 
 # Discard means if element is not found there will be no error
 num2 = {2,3,1,4}
-num2.discard(6)
+# num2.discard(6)
+# num2.pop()
+num2.clear()
 print(num2)
+
+
+
+# Set Method
+num3 = {2,3,1,4}
+num4 = {1,2,3,4,5}
+
+
+# num3.union(num4)
+print(num3.union(num4))   # ignore duplicate and add
+
+# num3.intersection(num4)
+print(num3.intersection(num4))   # common value between two set
