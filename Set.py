2a1acd7754b73a6dd4ad818da2f3c6b8d1a3bc5b
Rishab Raj <rr0948555@gmail.com>
2026-03-22 14:28:54 +0530

    Set

--- Set.py
+++ Set.py
@@ -1,7 +1,28 @@
 # A set is an unordered, mutable, and unique-elements-only collection in Python.
 # No Duplicates allowed
 
+
 # Creating
-my_set = {2,3,1,4}
+my_set = {2,3,1,4,2,1,6,"Alex","Alex"}
+
 print(my_set)
 print(type(my_set))
+print(len(my_set))
+
+
+
+# Empty set 
+my_set1 = set()
+
+print(my_set1)
+print(len(my_set1))
+
+
+
+# Empty set Adding Values
+my_set2 = set()
+my_set2.add(1)
+my_set2.add(2)
+
+print(my_set2)
+print(len(my_set2))
