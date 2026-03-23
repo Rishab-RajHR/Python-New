381e66ab4eb93a3ce798bd9b1f74759d3373d4a8
Rishab Raj <rr0948555@gmail.com>
2026-03-23 14:03:48 +0530

    While

---
+++ WhileLoop.py
@@ -0,0 +1,14 @@
+# loops are used to repeat a block of code multiple times ( repeatation )
+
+
+# Without Loop
+print(1)
+print(2)
+print(3)
+
+
+# While Loop => Used when you want to repeat something until a condition becomes false.
+num = 1
+while num <= 10:
+  print(num)
+  num += 1
