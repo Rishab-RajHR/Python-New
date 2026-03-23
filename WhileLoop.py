f6ab464606d93284c994f1db2797b43f680e63ea
Rishab Raj <rr0948555@gmail.com>
2026-03-23 14:13:09 +0530

    While Loop

--- WhileLoop.py
+++ WhileLoop.py
@@ -21,3 +21,33 @@ while num1 >= 1:
   print(num1)
   num1 -= 1
 print("Loop finished")
+
+
+
+# Infinite times loop will execute
+# while True:
+#    print("ALex Pandian")
+# print("End of Loop")
+
+
+
+# break statement terminates the loop
+i = 1
+while i <= 5:
+   print(i)
+   if i == 3:
+      break
+   i += 1
+print("Loop ended")
+
+
+# continue statement skips the current iteration
+
+j = 0
+while j < 5 :
+   if j == 3:
+      j += 1
+      continue
+   print(j)
+   j += 1
+print("Loop ended")
