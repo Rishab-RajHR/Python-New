59b6864306ef443b482cb437f036cd86788fa0c5
Rishab Raj <rr0948555@gmail.com>
2026-03-25 13:25:09 +0530

    Lambda

---
+++ LambdaFunction.py
@@ -0,0 +1,8 @@
+# A lambda function in Python is a small, anonymous (unnamed) function defined using the lambda keyword. It’s typically used for short, one-line operations where defining a full function using def is unnecessary.
+
+
+# lambda arguments: expression
+# f = lambda a: a*a
+
+add = lambda x,y : x+y
+print(add(10,20))
