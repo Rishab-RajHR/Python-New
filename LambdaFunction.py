511280b3e1f38b1b6f9c2279a3806b84579316ab
Rishab Raj <rr0948555@gmail.com>
2026-03-25 13:37:04 +0530

    Lambda

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -28,9 +28,17 @@ evens = list(filter(lambda x: x%2 == 0, nums1))
 print(evens)
 
 
+# Using loop
 nums2 = [1, 2, 3, 4]
 product = 1
 for num in nums2:
    product = product * num
 print(product)
+
+
 # Lambda function with reduce
+from functools import reduce
+
+nums3 = [1, 2, 3, 4]
+product1 = reduce(lambda x,y: x*y, nums3)
+print(product1)
