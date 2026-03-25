5045522965482a2c0cb475a7a7951f9518838d95
Rishab Raj <rr0948555@gmail.com>
2026-03-25 13:35:23 +0530

    Lambda

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -6,3 +6,31 @@
 
 add = lambda x,y : x+y
 print(add(10,20))
+
+
+# Normal Function
+def add_func(a,b):
+   return a+b
+print(add_func(10,20))
+
+
+
+# Lambda function with map
+nums = [1, 2, 3, 4, 5]
+# squared_nums = list(map(function,iteration))
+add = list(map(lambda x: x+2, nums))
+print(add)
+
+
+# Lambda function with filter
+nums1 = [1, 2, 3, 4, 5]
+evens = list(filter(lambda x: x%2 == 0, nums1))
+print(evens)
+
+
+nums2 = [1, 2, 3, 4]
+product = 1
+for num in nums2:
+   product = product * num
+print(product)
+# Lambda function with reduce
