25e36e9c8d92f5afe3d35e4620d197c6b68fa5e4
Rishab Raj <rr0948555@gmail.com>
2026-03-26 14:07:38 +0530

    Filehandling

---
+++ FileHandling/read.py
@@ -0,0 +1,6 @@
+# File handling in Python means working with files (create, read, write, update, delete). It’s a very common interview and practical topic.
+
+# To open a file
+f = open("test.txt","r")
+data = f.read()
+print(data)
