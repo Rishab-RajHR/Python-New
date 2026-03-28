451b988cde766b1092f01a6fb6590a29c1948e7b
Rishab Raj <rr0948555@gmail.com>
2026-03-28 22:31:53 +0530

    OOPS

--- OOPS.py
+++ OOPS.py
@@ -1,6 +1,60 @@
 # Object-Oriented Programming (OOP) is a programming style where you organize code using classes and objects.
 
-a=10
-b=20
-sum=a+b
-print("The sum of a and b is:", sum)
+# a=10
+# b=20
+# sum=a+b
+# print("The sum of a and b is:", sum)
+
+
+
+# Functional Programming
+# def hello():
+#   print("Hello, world!")
+
+# hello()
+
+
+
+# OOPS => Class and Object
+
+# class Student:
+#   name = "Alex Pandian"
+
+# s1 = Student()
+# print(s1.name)
+
+
+# class Car:
+#   color = "Red"
+#   modal = 2023
+
+# car2 = Car()
+# print(car2.color)
+# print(car2.modal)
+
+
+
+# constructor => __init__
+# class Person:
+#   def __init__(self):
+#     print("Person created")
+
+# p1 = Person()
+
+
+
+class Person:
+   def __init__(self,name,age):
+      self.name = name
+      self.age = age
+      print("Person Created")
+
+p1 = Person("Alex", 30)
+print(p1.name)
+print(p1.age)
+
+
+
+
+
+
