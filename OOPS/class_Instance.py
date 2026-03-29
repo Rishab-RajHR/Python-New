a928fe1416e292fba189f801fc6655a13283dd5a
Rishab Raj <rr0948555@gmail.com>
2026-03-29 14:43:10 +0530

    Instance

--- OOPS/class_Instance.py
+++ OOPS/class_Instance.py
@@ -16,3 +16,28 @@ s2 = Car("Safari", "Black")
 print(s2.model, s2.color)
 print(Car.car_company) # Accessing class attribute using instance
       
+
+
+
+# Methods are functions defined inside a class. They are used to perform operations on the data contained in the class. Methods can be called on instances of the class to manipulate or retrieve information about the instance.
+
+class Student:
+    def __init__ (self, name):
+        self.name = name
+
+    def hello(self):
+        print("Hello, self.name")
+
+s1 = Student("Alex")
+print(s1.name)
+s1.hello()
+
+
+
+# Static method => does not take self as parameter
+class Student:
+     @staticmethod #decorator
+     def college():
+         print("I am from DU")
+
+Student.college() # Calling static method without creating an instance
