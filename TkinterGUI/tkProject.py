44016b4632f3edf8f4f700e04b14d4c39d60c055
Rishab Raj <rr0948555@gmail.com>
2026-04-09 10:58:34 +0530

    Label

--- TkinterGUI/tkProject.py
+++ TkinterGUI/tkProject.py
@@ -3,7 +3,11 @@ import tkinter as tk
 # Create the main window
 root = tk.Tk()
 root.title("My First GUI Project")
-root.geometry('600*400')
+root.geometry('600x400')
+
+# Create a label
+label = tk.Label(root, text="Welcome to Alex GUI",font=("Arial", 16))
+label.pack()
 
 # Start GUI
 root.mainloop()
