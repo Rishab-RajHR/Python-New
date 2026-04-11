fd07aa2e217911603d7ef19fe501344800725fc2
Rishab Raj <rr0948555@gmail.com>
2026-04-11 10:56:15 +0530

    API

--- VENV/api.py
+++ VENV/api.py
@@ -1,8 +1,20 @@
 import requests
 
-url = "https://jsonplaceholder.typicode.com/posts/1"
+# url = "https://jsonplaceholder.typicode.com/posts/1"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-print("Status: ",response.status_code)
-print("Data: ",response.json())
+# print("Status: ",response.status_code)
+# print("Data: ",response.json())
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+new_post = {
+    "title": "Alex Pandian",
+    "body": "This is the post created by Alex.",
+    "userId": 1
+}
+
+response = requests.post(url, json=new_post)
+print("Status: ", response.status_code)
+print("Data: ", response.json())
