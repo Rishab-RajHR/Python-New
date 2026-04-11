e63818c882a35fb7258b30904c3c892ea9782c7b
Rishab Raj <rr0948555@gmail.com>
2026-04-11 10:51:26 +0530

    API

--- VENV/api.py
+++ VENV/api.py
@@ -4,5 +4,5 @@ url = "https://jsonplaceholder.typicode.com/posts/1"
 
 response = requests.get(url)
 
-print(response.status_code)
-print(response.json())
+print("Status: ",response.status_code)
+print("Data: ",response.json())
